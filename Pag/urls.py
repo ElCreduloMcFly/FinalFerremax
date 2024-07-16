b1b681d2136f9b711c71b1eda52749ab19ea8971
ElCreduloMcFly <gas.sanchez@duocuc.cl>
2024-07-16 15:00:14 -0400

    integracion webpay lista falta 1 o 2 cosas
    Poner bonitas las HTMLS de error y rechazado

---
+++ Pag/urls.py
@@ -0,0 +1,26 @@
+from django.contrib import admin
+from django.urls import path
+from .views import mostrarhome,mostrarcarrito,mostrarherramientas,mostrarinicio,mostrarvendedor,iniciarsesion,registrar,finsesion,mostraragregar,agregarproducto,eliminarproducto,modificarproducto,mostrarproducto,mostrarnuevotrabajador,mostrarmenulogin,error,rechazo,webpay_plus_commit,webpay_plus_create
+
+urlpatterns = [
+    path('',mostrarhome,name="MenuPrincipal"),
+    path('Carrito/',mostrarcarrito,name="Carrito"),
+    path('Herramientas/',mostrarherramientas,name="Herramientas"),
+    path('Inicio/',mostrarinicio,name="Sesion"),
+    path('Vendedor/',mostrarvendedor,name="Vendedor"),
+    path('MenuPrincipalLogin/',mostrarmenulogin,name="MenuPrincipalLogin"),
+    path('mostrarproducto/',mostraragregar,name="mostrarcategoria"),
+    path('mostrarnuevotrabajador/',mostrarnuevotrabajador,name="mostrarnuevotrabajador"),
+    path('iniciarsesion/', iniciarsesion, name='iniciarsesion'),   
+    path('registrar/',registrar,name="registrar"),
+    path('finsesion/',finsesion, name='finsesion'),
+    path('agregarproducto/',agregarproducto, name='agregarproducto'),
+    path('eliminarproducto/<id_prod>',eliminarproducto, name='eliminarproducto'),
+    path('mostrarproducto/<int:id_prod>',mostrarproducto, name='mostrarproducto'),
+    path('modificarproducto/<int:id_prod>',modificarproducto, name='modificarproducto'),
+    #WEBPAY
+    path('error-compra', error),
+    path('compra-rechazada', rechazo),
+    path('create-webpay', webpay_plus_create),
+    path('commit-webpay', webpay_plus_commit),
+]
